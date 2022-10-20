ae1c262a444ea4f0e4cb3cead438f47f8e4dd9d8
unknown <you@example.com>
2022-10-20 21:56:48 +0800

    MAIN

--- database/dbconfig2.php
+++ database/dbconfig2.php
@@ -1,10 +1,10 @@
 <?php
 	try {
 		// localhost
-		$pdoConnect = new PDO("mysql:host=localhost;dbname=dhvsu_hgdg", "root", "");
+		// $pdoConnect = new PDO("mysql:host=localhost;dbname=dhvsu_hgdg", "root", "");
 
 		// Live
-		// $pdoConnect = new PDO("mysql:host=localhost;dbname=u867039073_hgdg", "u867039073_hgdg", "Andreishania12");
+		$pdoConnect = new PDO("mysql:host=localhost;dbname=u867039073_hgdg", "u867039073_hgdg", "Andreishania12");
 		$pdoConnect->setAttribute(PDO:: ATTR_ERRMODE, PDO:: ERRMODE_EXCEPTION);
 
 	}
