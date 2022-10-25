1e691048df87918c2865158028cce4409219ceeb
unknown <you@example.com>
2022-10-25 22:19:30 +0800

    MAIN

--- dashboard/superadmin/home.php
+++ dashboard/superadmin/home.php
@@ -126,7 +126,7 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
     <!-- Content -->
     <div class="card-boxes">
       <div class="box">
-        <div class="right_side">
+        <div class="right_side" nclick="location.href='admin'">
             <?php
                 $pdoQuery = "SELECT * FROM admin";
                 $pdoResult1 = $pdoConnect->prepare($pdoQuery);
